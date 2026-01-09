b2f3226568dbb504d7080629a554c00c13371859
G.S.Chandhru <gschandhru10@gmail.com>
2026-01-09 21:19:47 +0530

    feat(array) : added moveZoroes sum

---
+++ Array/moveZeroes.js
@@ -0,0 +1,22 @@
+var moveZeroes = function(nums) {
+    let nonZeroElements = [];
+    let zeroElements = [];
+
+    for (let value of nums) {
+        if (value !== 0) {
+            nonZeroElements.push(value);
+        } else {
+            zeroElements.push(0);
+        }
+    }
+
+    // append zeros at the end
+    for (let zero of zeroElements) {
+        nonZeroElements.push(zero);
+    }
+
+    // modify nums in-place
+    for (let i = 0; i < nums.length; i++) {
+        nums[i] = nonZeroElements[i];
+    }
+};
