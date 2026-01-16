c2449e56c5734013782bb7185c0e1e78e12b6fc2
G.S.Chandhru <gschandhru10@gmail.com>
2026-01-16 17:27:31 +0530

    feat(math) : added convertToBase7 sum

---
+++ Math/convertToBase7.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number} num
+ * @return {string}
+ */
+var convertToBase7 = function (num) {
+    if (num === 0) return "0";
+
+    let isNegative = num < 0;
+    num = Math.abs(num);
+
+    let result = "";
+
+    while (num > 0) {
+        result = (num % 7) + result;
+        num = Math.floor(num / 7);
+    }
+
+    return isNegative ? "-" + result : result;
+};
