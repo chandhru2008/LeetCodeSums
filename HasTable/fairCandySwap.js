787794d43435c9b92d4a3875812eec295e68af96
G.S.Chandhru <gschandhru10@gmail.com>
2026-01-19 16:51:38 +0530

    feat(hasTable) : added fairCandySwap sum

---
+++ HasTable/fairCandySwap.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number[]} aliceSizes
+ * @param {number[]} bobSizes
+ * @return {number[]}
+ */
+var fairCandySwap = function (aliceSizes, bobSizes) {
+    const sumA = aliceSizes.reduce((a, b) => a + b, 0);
+    const sumB = bobSizes.reduce((a, b) => a + b, 0);
+
+    const diff = (sumB - sumA) / 2;
+
+    const bobSet = new Set(bobSizes);
+
+    for (let x of aliceSizes) {
+        const y = x + diff;
+        if (bobSet.has(y)) {
+            return [x, y];
+        }
+    }
+};
+
+console.log(fairCandySwap([1, 1], [2, 2])); // [1,2]
+console.log(fairCandySwap([1, 2], [2, 3]));
